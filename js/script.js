b6eb056c1f9add8268424a93534dad1f6396f1b1
mielbekaert69-byte <mielbekaert69@gmail.com>
2026-06-03 17:47:14 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -65,3 +65,38 @@ biologicalAge -= 3;
 document.getElementById("result").innerHTML =
 `Geschatte biologische leeftijd: ${biologicalAge} jaar`;
 }
+const counters =
+document.querySelectorAll(".counter");
+
+counters.forEach(counter => {
+
+const updateCounter = () => {
+
+const target =
++counter.getAttribute("data-target");
+
+const count =
++counter.innerText;
+
+const increment =
+target / 100;
+
+if(count < target){
+
+counter.innerText =
+Math.ceil(count + increment);
+
+setTimeout(updateCounter,20);
+
+}
+else{
+
+counter.innerText = target;
+
+}
+
+};
+
+updateCounter();
+
+});
